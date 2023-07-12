75b861aed5240f757395b6ba97a920664bb01d56
NSINGHRAJPUT <nsinghrajput30@gmail.com>
2023-07-12 10:53:00 +0530

    requestandresponse

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
+  const url = req.url;
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
-  res.write("<body> Neeraj </body>");
+  if (url == "/home") {
+    res.write("<body> Welcome Home </body>");
+  } else if (url == "/about") {
+    res.write("<body> Welcome to about us page </body>");
+  } else if (url == "/node") {
+    res.write("<body> Welcome to my Node JS project </body>");
+  } else {
+    res.write("<body> Sorry!!!! Wrong request</body>");
+  }
   res.write("</html>");
+  res.end();
 });
 console.log("Neeraj");
 server.listen(4000);
