f19ddd9c87547eeb642a31b3edd4824beaf37399
NSINGHRAJPUT <nsinghrajput30@gmail.com>
2023-07-20 15:09:34 +0530

    middleware

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
-const http = require("http");
-const requestHandler = require("./requestHandler");
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const server = http.createServer(requestHandler);
+const app = express();
+app.use(bodyParser.urlencoded())
 
-server.listen(4000);
+app.use('/add-product',(req,res,next)=>{
+    console.log('in the middleware')
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="quantity"><button type="submit">submit</button></form>')
+})
+
+app.use('/product', (req,res,next)=>{
+    console.log(req.body);
+    console.log(req.body.title);
+    console.log(req.body.quantity);
+    res.redirect('/')
+})
+
+app.use('/',(req,res,next)=>{
+    console.log('in another middleware')
+    res.send('<h1>Hello from Express</h1>')
+})
+
+
+app.listen(4000);
