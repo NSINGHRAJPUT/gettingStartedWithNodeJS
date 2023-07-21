2787a5e547389640ae9509188676213cfb704624
NSINGHRAJPUT <nsinghrajput30@gmail.com>
2023-07-21 15:02:27 +0530

    filtering routes

--- routes/admin.js
+++ routes/admin.js
@@ -6,7 +6,7 @@ router.get('/add-product',(req,res,next)=>{
     res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="quantity"><button type="submit">submit</button></form>')
 })
 
-router.post('/product', (req,res,next)=>{
+router.post('/add-product', (req,res,next)=>{
     console.log(req.body);
     console.log(req.body.title);
     console.log(req.body.quantity);
